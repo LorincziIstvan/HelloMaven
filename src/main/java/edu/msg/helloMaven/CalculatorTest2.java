a7ccb371e361a4e13b1f03abbc7ef4217cbd4e24
LorincziIstvan <lorinczi_isti@yahoo.com>
2014-09-03 16:11:25 +0300

    TestSuite

---
+++ src/main/java/edu/msg/helloMaven/CalculatorTest2.java
@@ -0,0 +1,37 @@
+package edu.msg.helloMaven;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest2 {
+
+	
+	
+
+	@Test
+	public void testAdd() {
+		assertEquals(Calculator.add(1,2),3);
+	}
+
+	@Test
+	public void testSub() {
+		assertEquals(Calculator.sub(3,2),1);
+	}
+
+	@Test
+	public void testMul() {
+		assertEquals(Calculator.mul(2,2),4);
+	}
+
+	@Test
+	public void testDiv() {
+		assertEquals(Calculator.div(1,2),0.5,0.00001);
+	}
+
+	@Test
+	public void testMod() {
+		assertEquals(Calculator.mod(1,2),1);
+	}
+	
+}
