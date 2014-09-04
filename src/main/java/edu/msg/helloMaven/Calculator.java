7f4c5e2afa1f9ef4c96f4e27db016b95afd6e139
LorincziIstvan <lorinczi_isti@yahoo.com>
2014-09-04 12:28:42 +0300

    Revert "wrongtestcase"
    This reverts commit 4231de036a470410cd31b37150d4427fcb79e41a.

--- src/main/java/edu/msg/helloMaven/Calculator.java
+++ src/main/java/edu/msg/helloMaven/Calculator.java
@@ -3,7 +3,7 @@ package edu.msg.helloMaven;
 public class Calculator {
 
 	public static int add(int a, int b) {
-		return a+b+1;
+		return a+b;
 	}
 	public static int sub(int a, int b) {
 		return a-b;
