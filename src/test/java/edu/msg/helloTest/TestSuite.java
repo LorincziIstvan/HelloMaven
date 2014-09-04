ab650a76a0c614bc054de79f9249c25e18909e59
LorincziIstvan <lorinczi_isti@yahoo.com>
2014-09-04 12:36:24 +0300

    Revert "no more testsuite"
    This reverts commit ec0a177f718cf575f5b380038472c8171aa9a381.

---
+++ src/test/java/edu/msg/helloTest/TestSuite.java
@@ -0,0 +1,12 @@
+package edu.msg.helloTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ CalcultaorTest.class, CalculatorTest2.class })
+public class TestSuite {
+
+} 
