4231de036a470410cd31b37150d4427fcb79e41a
LorincziIstvan <lorinczi_isti@yahoo.com>
2014-09-04 12:25:52 +0300

    wrongtestcase

--- src/main/java/edu/msg/helloMaven/Calculator.java
+++ src/main/java/edu/msg/helloMaven/Calculator.java
@@ -3,7 +3,7 @@ package edu.msg.helloMaven;
 public class Calculator {
 
 	public static int add(int a, int b) {
-		return a+b;
+		return a+b+1;
 	}
 	public static int sub(int a, int b) {
 		return a-b;
