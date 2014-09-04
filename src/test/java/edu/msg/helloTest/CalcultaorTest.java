281bb6f41c4611ff249a12260537d5e0008bdbe4
LorincziIstvan <lorinczi_isti@yahoo.com>
2014-09-04 12:33:39 +0300

    new test?

--- src/test/java/edu/msg/helloTest/CalcultaorTest.java
+++ src/test/java/edu/msg/helloTest/CalcultaorTest.java
@@ -11,8 +11,10 @@ public class CalcultaorTest {
 	
 	@Test
 	public void testAdd() {
-		System.out.println("test are running");
 		assertEquals(Calculator.add(1,2),3);
+	}
+	public void testAddTwo() {
+		assertEquals(Calculator.add(-1,2),1);
 	}
 
 	@Test
